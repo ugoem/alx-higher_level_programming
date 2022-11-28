f63b6d792e6373d7dfcc26c71456ada1144c3792
Anionovo Ugochukwu E <esuneteng.tech@gmail.com>
2022-11-28 18:18:33 +0100

    Update 5-print_string.py

--- 0x00-python-hello_world/5-print_string.py
+++ 0x00-python-hello_world/5-print_string.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-# Code to print 3 times a string stored in the variable str, followed by its first 9 characters.
 str = "Holberton School"
 print(f"{str}{str}{str}")
 print(f"{str[:10]}")
